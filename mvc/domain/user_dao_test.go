2bb52bb886a9999e1786aff94c22f52f62964b73
Tanmaya Gupta <tanmaya@tarkalabs.com>
2020-01-06 19:21:55 +0530

    Add test case for User with valid ID

--- mvc/domain/user_dao_test.go
+++ mvc/domain/user_dao_test.go
@@ -12,7 +12,19 @@ func TestUserNotFound(t *testing.T) {
 
 	assert.Nil(t, user, "we were not expecting a user with id 0")
 	assert.NotNil(t, err)
+
 	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
 	assert.EqualValues(t, "not_found", err.Code)
 	assert.EqualValues(t, "user 0 does not exist", err.Message)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, 123, user.ID)
+	assert.EqualValues(t, "Fede", user.FirstName)
+	assert.EqualValues(t, "Leon", user.LastName)
+	assert.EqualValues(t, "myemail@gmail.com", user.Email)
+
 }
