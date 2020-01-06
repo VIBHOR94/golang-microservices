88636d83ed0ae164c64c23ffe48db4cc42460fdd
Tanmaya Gupta <tanmaya@tarkalabs.com>
2020-01-06 22:28:17 +0530

    Add test case for bubble sort util

---
+++ mvc/utils/sort_utils_test.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBubbleSortWorstCase(t *testing.T) {
+	els := []int{9, 8, 7, 6, 5}
+
+	BubbleSort(els)
+	assert.NotNil(t, els)
+	assert.EqualValues(t, 5, len(els))
+	assert.EqualValues(t, 5, els[0])
+	assert.EqualValues(t, 6, els[1])
+	assert.EqualValues(t, 7, els[2])
+	assert.EqualValues(t, 8, els[3])
+	assert.EqualValues(t, 9, els[4])
+}
+
+func TestBubbleSortBestCase(t *testing.T) {
+	els := []int{5, 6, 7, 8, 9}
+
+	BubbleSort(els)
+	assert.NotNil(t, els)
+	assert.EqualValues(t, 5, len(els))
+	assert.EqualValues(t, 5, els[0])
+	assert.EqualValues(t, 6, els[1])
+	assert.EqualValues(t, 7, els[2])
+	assert.EqualValues(t, 8, els[3])
+	assert.EqualValues(t, 9, els[4])
+}
