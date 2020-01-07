1c272127ca8c6f629c06185048703836091fe643
Tanmaya Gupta <tanmaya@tarkalabs.com>
2020-01-07 18:51:40 +0530

    Add json parser to CreateRepoRequest struct

--- src/domain/github/create_repo.go
+++ src/domain/github/create_repo.go
@@ -1,11 +1,12 @@
 package github
 
+// CreateRepoRequest - Body for Creating Github repository via api service
 type CreateRepoRequest struct {
-	Name        string
-	Description string
-	Homepage    string
-	Private     bool
-	HasIssues   bool
-	HasProjects bool
-	HasWiki     bool
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Homepage    string `json:"homepage"`
+	Private     bool   `json:"private"`
+	HasIssues   bool   `json:"has_issues"`
+	HasProjects bool   `json:"has_projects"`
+	HasWiki     bool   `json:"has_wiki"`
 }
