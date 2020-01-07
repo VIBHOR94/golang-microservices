a58e12bff19d1e7b7f0c7dae1fa9c3e9dd57966b
Tanmaya Gupta <tanmaya@tarkalabs.com>
2020-01-07 19:39:51 +0530

    Add more CreateResponse, RepoOwner and RepoPermissions struct

--- src/domain/github/create_repo.go
+++ src/domain/github/create_repo.go
@@ -10,3 +10,27 @@ type CreateRepoRequest struct {
 	HasProjects bool   `json:"has_projects"`
 	HasWiki     bool   `json:"has_wiki"`
 }
+
+// CreateRepoResponse - Response Body recieved after Creating Github repository via api service
+type CreateRepoResponse struct {
+	ID          int64           `json:"id"`
+	Name        string          `json:"name"`
+	FullName    string          `json:"full_name"`
+	Owner       RepoOwner       `json:"owner"`
+	Permissions RepoPermissions `json:"permissions"`
+}
+
+// RepoOwner - Struct for details of repo owner
+type RepoOwner struct {
+	ID      int64  `json:"id"`
+	Login   string `json:"login"`
+	URL     string `json:"url"`
+	HTMLURL string `json:"html_url"`
+}
+
+// RepoPermissions - Struct for details of repo permission
+type RepoPermissions struct {
+	IsAdmin bool `json:"admin"`
+	HasPull bool `json:"push"`
+	HasPush bool `json:"pull"`
+}
