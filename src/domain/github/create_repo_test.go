fbbdc71608065b26003049ed6f388c2b987c04e0
Tanmaya Gupta <tanmaya@tarkalabs.com>
2020-01-07 19:20:33 +0530

    Add test case for checking CreateRepoRequest to json conversion

---
+++ src/domain/github/create_repo_test.go
@@ -0,0 +1,37 @@
+package github
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateRepoRequestAsJson(t *testing.T) {
+	request := CreateRepoRequest{
+		Name:        "golang introduction",
+		Description: "a golang introduction repository",
+		Homepage:    "https://github.com",
+		Private:     true,
+		HasIssues:   true,
+		HasProjects: true,
+		HasWiki:     true,
+	}
+
+	bytes, err := json.Marshal(request)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, bytes)
+	assert.EqualValues(t,
+		`{"name":"golang introduction","description":"a golang introduction repository","homepage":"https://github.com","private":true,"has_issues":true,"has_projects":true,"has_wiki":true}`,
+		string(bytes))
+
+	var target CreateRepoRequest
+	// Unmarshal takes an input byte array and attempts to create a valid json string.
+	err = json.Unmarshal(bytes, &target)
+
+	assert.Nil(t, err)
+
+	assert.EqualValues(t, target.Name, request.Name)
+	assert.EqualValues(t, target.HasIssues, request.HasIssues)
+}
