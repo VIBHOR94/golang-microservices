ef881836baf03ceb97f3c146dab4e4278eb94467
Tanmaya Gupta <tanmaya@tarkalabs.com>
2020-01-08 19:04:06 +0530

    Create function Post in restclient.go to return a http response anfter post request

---
+++ src/api/clients/restclient/restclient.go
@@ -0,0 +1,20 @@
+package restclient
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+// Post - Take the URL and body to make a post request
+func Post(url string, body interface{}, headers http.Header) (*http.Response, error) {
+	jsonBytes, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(jsonBytes))
+	request.Header = headers
+
+	client := http.Client{}
+	return client.Do(request)
+}
