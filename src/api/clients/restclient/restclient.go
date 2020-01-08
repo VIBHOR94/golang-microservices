791930284fc0a7d6b1a577c7f73573f08c19d5a6
Tanmaya Gupta <tanmaya@tarkalabs.com>
2020-01-08 20:21:45 +0530

    Add mocking functions to restclient to use it in github_provider test cases

--- src/api/clients/restclient/restclient.go
+++ src/api/clients/restclient/restclient.go
@@ -3,11 +3,52 @@ package restclient
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 )
 
-// Post - Take the URL and body to make a post request
+var (
+	enabledMocks = false
+	mocks        = make(map[string]*Mock)
+)
+
+type Mock struct {
+	Url        string
+	HttpMethod string
+	Response   *http.Response
+	Err        error
+}
+
+func getMockId(httpMethod string, url string) string {
+	return fmt.Sprintf("%s_%s", httpMethod, url)
+}
+
+func StartMockups() {
+	enabledMocks = true
+}
+
+func FlushMockups() {
+	mocks = make(map[string]*Mock)
+}
+
+func StopMockups() {
+	enabledMocks = false
+}
+
+func AddMockup(mock Mock) {
+	mocks[getMockId(mock.HttpMethod, mock.Url)] = &mock
+}
+
 func Post(url string, body interface{}, headers http.Header) (*http.Response, error) {
+	if enabledMocks {
+		mock := mocks[getMockId(http.MethodPost, url)]
+		if mock == nil {
+			return nil, errors.New("no mockup found for give request")
+		}
+		return mock.Response, mock.Err
+	}
+
 	jsonBytes, err := json.Marshal(body)
 	if err != nil {
 		return nil, err
