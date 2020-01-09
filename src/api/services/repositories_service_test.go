af38512372fbe6bd15c33c870318bb8be138aa24
VIBHOR94 <vibhorgupta2008@aol.in>
2020-01-09 17:04:54 +0530

    Add test case when creating github repo throws an error

--- src/api/services/repositories_service_test.go
+++ src/api/services/repositories_service_test.go
@@ -1,8 +1,10 @@
 package services
 
 import (
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,4 +26,23 @@ func TestCreateRepoInvalidInputNmae(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.EqualValues(t, http.StatusBadRequest, err.Status())
 	assert.EqualValues(t, "invalid repository name", err.Message())
+}
+
+func TestCreateRepoErrorFromGithub(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"message": "Requires authentication","documentation_url": "https://developer.github.com/docs"}`)),
+		},
+	})
+	request := repositories.CreateRepoRequest{Name: "testing"}
+
+	result, err := RepositoryService.CreateRepo(request)
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusUnauthorized, err.Status())
+	assert.EqualValues(t, "Requires authentication", err.Message())
 }
