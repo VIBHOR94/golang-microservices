1d2464f066da579e5877bb8918884b4dd5f344e0
VIBHOR94 <vibhorgupta2008@aol.in>
2020-01-11 01:39:04 +0530

    Add function to create multiple repositories in one request - Change endpoint '/repositories' to '/repository' for creation of one single repository - Create endpoint '/repositories' for creating multiple repositories in one go - Use concurrency, channels to create the repository - If all repository request sent in body is falied return error of first failed one - If at least one is created set status to partially created

--- src/api/services/repository_service.go
+++ src/api/services/repository_service.go
@@ -1,7 +1,8 @@
 package services
 
 import (
-	"strings"
+	"net/http"
+	"sync"
 
 	githubprovider "github.com/VIBHOR94/golang-microservices/src/api/providers/github_provider"
 
@@ -16,6 +17,7 @@ type resposService struct{}
 
 type reposServiceInterface interface {
 	CreateRepo(repositories repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.APIError)
+	CreateRepos(repositories []repositories.CreateRepoRequest) (repositories.CreateReposResponse, errors.APIError)
 }
 
 var (
@@ -27,11 +29,9 @@ func init() {
 }
 
 func (s *resposService) CreateRepo(input repositories.CreateRepoRequest) (*repositories.CreateRepoResponse, errors.APIError) {
-	input.Name = strings.TrimSpace(input.Name)
-	if input.Name == "" {
-		return nil, errors.NewBadRequestError("invalid repository name")
+	if err := input.Validate(); err != nil {
+		return nil, err
 	}
-
 	request := github.CreateRepoRequest{
 		Name:        input.Name,
 		Description: input.Description,
@@ -49,4 +49,67 @@ func (s *resposService) CreateRepo(input repositories.CreateRepoRequest) (*repos
 		Owner: response.Owner.Login,
 	}
 	return &result, nil
+}
+
+func (s *resposService) CreateRepos(requests []repositories.CreateRepoRequest) (repositories.CreateReposResponse, errors.APIError) {
+	input := make(chan repositories.CreateRepositoriesResult)
+	output := make(chan repositories.CreateReposResponse)
+
+	defer close(output)
+
+	var wg sync.WaitGroup
+	go s.handleRepoResults(&wg, input, output)
+
+	for _, current := range requests {
+		wg.Add(1)
+		go s.createRepoConcurrent(current, input)
+	}
+
+	wg.Wait()
+	close(input)
+
+	result := <-output
+
+	successCreations := 0
+	for _, current := range result.Results {
+		if current.Response != nil {
+			successCreations++
+		}
+	}
+
+	if successCreations == 0 {
+		result.StatusCode = result.Results[0].Error.Status()
+	} else if successCreations == len(requests) {
+		result.StatusCode = http.StatusCreated
+	} else {
+		result.StatusCode = http.StatusPartialContent
+	}
+	return result, nil
+}
+
+func (s *resposService) handleRepoResults(wg *sync.WaitGroup, input chan repositories.CreateRepositoriesResult, output chan repositories.CreateReposResponse) {
+	var results repositories.CreateReposResponse
+	for incomingEvent := range input {
+		repoResult := repositories.CreateRepositoriesResult{
+			Response: incomingEvent.Response,
+			Error:    incomingEvent.Error,
+		}
+		results.Results = append(results.Results, repoResult)
+		wg.Done()
+	}
+	output <- results
+}
+
+func (s *resposService) createRepoConcurrent(input repositories.CreateRepoRequest, output chan repositories.CreateRepositoriesResult) {
+	if err := input.Validate(); err != nil {
+		output <- repositories.CreateRepositoriesResult{Error: err}
+		return
+	}
+	result, err := s.CreateRepo(input)
+	if err != nil {
+		output <- repositories.CreateRepositoriesResult{Error: err}
+		return
+	}
+	output <- repositories.CreateRepositoriesResult{Response: result}
+
 }
