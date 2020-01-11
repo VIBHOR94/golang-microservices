170db937835eda4e462f4e5cd403c742fb6e91d1
VIBHOR94 <vibhorgupta2008@aol.in>
2020-01-11 17:08:39 +0530

    Add file to handle millions of create repo request using concurrency

---
+++ concurrency/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/VIBHOR94/golang-microservices/src/api/utils/errors"
+
+	"github.com/VIBHOR94/golang-microservices/src/api/domain/repositories"
+	"github.com/VIBHOR94/golang-microservices/src/api/services"
+)
+
+var (
+	success = make(map[string]string, 0)
+	failed  = make(map[string]errors.APIError, 0)
+)
+
+type createRepoResult struct {
+	Request repositories.CreateRepoRequest
+	Result  *repositories.CreateRepoResponse
+	Error   errors.APIError
+}
+
+func getRequests() []repositories.CreateRepoRequest {
+	result := make([]repositories.CreateRepoRequest, 0)
+
+	file, err := os.Open("/Users/tanmaya/Desktop/requests/requests.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		request := repositories.CreateRepoRequest{
+			Name: line,
+		}
+		result = append(result, request)
+	}
+	return result
+}
+
+func main() {
+	requests := getRequests()
+
+	fmt.Println(fmt.Sprintf("about to process %d requests", len(requests)))
+
+	input := make(chan createRepoResult)
+	buffer := make(chan bool, 10)
+	var wg sync.WaitGroup
+
+	go handleResults(&wg, input)
+
+	for _, request := range requests {
+		buffer <- true
+		wg.Add(1)
+		go createRepo(buffer, input, request)
+	}
+
+	wg.Wait()
+	close(input)
+
+	// Now you can write success and failed maps to disk or notify them via email or anything you need to do.
+}
+
+func handleResults(wg *sync.WaitGroup, input chan createRepoResult) {
+	for result := range input {
+		if result.Error != nil {
+			failed[result.Request.Name] = result.Error
+		} else {
+			success[result.Request.Name] = result.Result.Name
+		}
+		wg.Done()
+	}
+}
+
+func createRepo(buffer chan bool, output chan createRepoResult, request repositories.CreateRepoRequest) {
+	result, err := services.RepositoryService.CreateRepo(request)
+
+	output <- createRepoResult{
+		Request: request,
+		Result:  result,
+		Error:   err,
+	}
+	<-buffer
+}
